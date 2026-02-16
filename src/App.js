78e4f23f255acc23664fefa65d6b097dd766f701
yejishin-hub <sinyeji958@gmail.com>
2026-02-16 17:38:26 +0900

    2026-02-16 React 7-6. localStorage로 데이터 보관하기 - localStorage vs SessionStorage - localStorage는 문자열 저장만 가능 (객체 저장/조회 필요 시 JSON.stringify / JSON.parse 활용) - debounce 라이브러리 (Lodash) 활용한 localStorage setItem 호출 최소화

--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.css';
 import MemoContainer from './components/MemoContainer';
 import SideBar from './components/SideBar';
+import { getItem, setItem } from './lib/storage';
+import debounce from 'lodash.debounce';
+
+// setItem이 1번 이상 호출될 때, 마지막 호출로부터 5초가 지나면 setItem을 실행한다.
+const debouncedSetItem = debounce(setItem, 5000);
 
 function App() {
-  const [memos, setMemos] = useState([
-    {
-      title: 'Memo 1',
-      content: 'This is memo 1',
-      createdAt: 1770733338339, // 시간 값
-      updatedAt: 1770733338339, // 시간 값
-    },
-    {
-      title: 'Memo 2',
-      content: 'This is memo 2',
-      createdAt: 1770733364798, // 시간 값
-      updatedAt: 1770733364798, // 시간 값
-    },
-  ]);
+  const [memos, setMemos] = useState(getItem('memo') || []);
 
   const [selectedMemoIndex, setSelectedMemoIndex] = useState(0);
 
-  const setMemo = (newMemo) => {
-    // 불변성 어긴 코드
-    // memos[selectedMemoIndex] = newMemo;
-    // 1) reference 안의 리렌더링 X
-    // setMemos(memos);
-    // 2) 객체 업데이트 판단 -> 리렌더링 O
-    // setMemos([...memos]);
+  const setMemo = useCallback(
+    (newMemo) => {
+      // 불변성 어긴 코드
+      // memos[selectedMemoIndex] = newMemo;
+      // 1) reference 안의 리렌더링 X
+      // setMemos(memos);
+      // 2) 객체 업데이트 판단 -> 리렌더링 O
+      // setMemos([...memos]);
 
-    // 불변성 유지
-    const newMemos = [...memos];
-    newMemos[selectedMemoIndex] = newMemo;
-    setMemos(newMemos);
-  };
+      // 불변성 유지
+      // const newMemos = [...memos];
+      // newMemos[selectedMemoIndex] = newMemo;
+      // setMemos(newMemos);
+      // state : memos
+      setMemos((memos) => {
+        const newMemos = [...memos];
+        newMemos[selectedMemoIndex] = newMemo;
+        debouncedSetItem('memo', newMemos);
+        return newMemos;
+      });
+    },
+    [/*memos,*/ selectedMemoIndex], // memos와 selectedMemoIndex가 바뀌는 경우 setMemo 재생성
+  );
 
-  const addMemo = () => {
-    const now = new Date().getTime();
-
-    setMemos([
-      ...memos,
-      {
-        title: 'Untitled',
-        content: '',
-        createdAt: now,
-        updatedAt: now,
-      },
-    ]);
+  const addMemo = useCallback(() => {
+    // setMemos(newMemos);
+    setMemos((memos) => {
+      const now = new Date().getTime();
+      const newMemos = [
+        ...memos,
+        {
+          title: 'Untitled',
+          content: '',
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+      debouncedSetItem('memo', newMemos);
+      return newMemos;
+    });
     setSelectedMemoIndex(memos.length); // 추가한 메모 선택
-  };
+  }, [memos]);
 
-  const deleteMemo = (index) => {
-    const newMemos = [...memos];
-    newMemos.splice(index, 1);
-    setMemos(newMemos);
-    if (index === selectedMemoIndex) {
-      setSelectedMemoIndex(0);
-    }
-  };
+  const deleteMemo = useCallback(
+    (index) => {
+      // setMemos(newMemos);
+      setMemos((memos) => {
+        const newMemos = [...memos];
+        newMemos.splice(index, 1);
+        debouncedSetItem('memo', newMemos);
+        return newMemos;
+      });
+      if (index === selectedMemoIndex) {
+        setSelectedMemoIndex(0);
+      }
+    },
+    // React Hook useCallback has an unnecessary dependency: 'memos'. Either exclude it or remove the dependency array
+    [/*memos,*/ selectedMemoIndex],
+  );
 
   return (
     <div className="App">
